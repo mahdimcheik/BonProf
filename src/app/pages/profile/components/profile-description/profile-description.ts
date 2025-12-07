2a506e3ae43610a431f6ed862d2ea81f78f2287c
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-12-07 17:38:41 +0100

    design profil

--- src/app/pages/profile/components/profile-description/profile-description.ts
+++ src/app/pages/profile/components/profile-description/profile-description.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { Tag } from 'primeng/tag';
 
 @Component({
     selector: 'bp-profile-description',
-    imports: [],
+    imports: [Card, Button, Tag],
     templateUrl: './profile-description.html'
 })
 export class ProfileDescription {}
--- src/app/pages/profile/components/profile-infos/profile-infos.ts
+++ src/app/pages/profile/components/profile-infos/profile-infos.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
 import { Image } from 'primeng/image';
+import { Tag } from 'primeng/tag';
 
 @Component({
     selector: 'bp-profile-infos',
-    imports: [Image, Button],
+    imports: [Image, Button, Card, Tag],
     templateUrl: './profile-infos.html'
 })
 export class ProfileInfos {}
