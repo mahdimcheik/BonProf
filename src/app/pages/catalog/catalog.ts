96471064910260b8121808c2f932d18699aa2867
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-01-08 19:40:39 +0100

    precedent

--- src/app/pages/catalog/catalog.ts
+++ src/app/pages/catalog/catalog.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TabsModule } from 'primeng/tabs';
+import { CursusesList } from '../cursuses/cursuses-list/cursuses-list';
+import { ProductsList } from '../products/products-list/products-list';
+import { MainService } from '../shared/services/main.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'bp-catalog',
-  imports: [],
-  templateUrl: './catalog.html',
-  styleUrl: './catalog.scss',
+    selector: 'bp-catalog',
+    imports: [TabsModule, CursusesList, ProductsList],
+    templateUrl: './catalog.html'
 })
 export class Catalog {
+    mainService = inject(MainService);
+    router = inject(Router);
+    activatedRoute = inject(ActivatedRoute);
+    tab: string = 'cursuses';
 
+    ngOnInit() {
+        this.mainService.getTeacherFullProfile();
+        // .subscribe();
+        this.activatedRoute.queryParams.subscribe((params) => {
+            const tabParam = params['tab'];
+            if (tabParam) {
+                this.tab = tabParam;
+            } else {
+                this.tab = 'cursuses';
+            }
+        });
+    }
+
+    onTabChange(tabValue: string | number | undefined) {
+        this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams: { tab: tabValue },
+            queryParamsHandling: 'merge'
+        });
+    }
 }
