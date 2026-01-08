ea40d07d6e575327c423480b51891b79c039f2d8
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-01-08 17:38:45 +0100

    libremap

--- src/app/pages/profile/components/address/map-basic.ts
+++ src/app/pages/profile/components/address/map-basic.ts
@@ -1,108 +1,48 @@
-import { AfterViewInit, Component, computed, input, OnDestroy } from '@angular/core';
-import { AddressDetails, UserDetails } from 'src/client';
-import * as L from 'leaflet';
-
-// 1. On importe le CSS du plugin (si ce n'est pas fait dans angular.json)
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-
-// 2. L'astuce cruciale :
-// Certains plugins (comme markercluster) dépendent de l'existence de "window.L".
-// On attache manuellement notre instance importée à l'objet window.
-// (Il faut parfois caster window en 'any' pour TypeScript)
-(window as any).L = L;
-
-// 3. On importe le JS du plugin APRÈS avoir défini window.L
-import 'leaflet.markercluster';
+import { AfterViewInit, ChangeDetectorRef, Component, computed, effect, inject, input, OnDestroy } from '@angular/core';
+import { AddressDetails } from 'src/client';
+import * as maplibregl from 'maplibre-gl';
+import { LayoutService } from '@/layout/service/layout.service';
 
 @Component({
     selector: 'bp-map-basic',
     imports: [],
     styleUrls: ['./map-basic.scss'],
-    template: '@if(mainAddress().latitude && mainAddress().longitude){<div [id]="mapId" class=" rounded-xl min-h-[200px] w-full min-w-[300px]"></div>}'
+    template: '@if(mainAddress().latitude && mainAddress().longitude){<div id="map" class=" rounded-xl min-h-[200px] w-full min-w-[300px]"></div>}'
 })
 export class MapBasic implements AfterViewInit, OnDestroy {
-    user = input.required<UserDetails>();
+    layoutService = inject(LayoutService);
+    cdr = inject(ChangeDetectorRef);
     mainAddress = input.required<AddressDetails>();
     secondaryAddress = input<AddressDetails | null>(null);
 
-    // Generate unique map ID for each instance
-    mapId = `map-${Math.random().toString(36).substr(2, 9)}`;
+    latitude = computed(() => this.mainAddress().latitude ?? 48.8566);
+    longitude = computed(() => this.mainAddress().longitude ?? 2.3522);
+    zoom = 14;
 
-    data = computed<AddressDetails[]>(() => {
-        const mainAddress = this.mainAddress();
-        const secondaryAddress = this.secondaryAddress();
-
-        var addresses: AddressDetails[] = [];
-        addresses.push(mainAddress);
-        if (secondaryAddress) {
-            addresses.push(secondaryAddress);
-        }
-        return addresses;
+    map!: maplibregl.Map;
+    mapUrl = computed(() => {
+        const isDark = this.layoutService.isDarkTheme();
+        return isDark ? 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json' : 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
     });
 
-    private map!: L.Map;
-    private markersLayer!: L.MarkerClusterGroup;
-    private mapInitialized = false;
-
-    ngAfterViewInit(): void {
-        this.markersLayer = L.markerClusterGroup();
-        this.initMap();
-        this.mapInitialized = true;
-        this.loadMarkers();
-        this.fitMapToMarkers();
-    }
-
-    constructor() {}
-
-    private initMap(): void {
-        this.map = L.map(this.mapId).setView([48.8566, 2.3522], 64);
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap'
-        }).addTo(this.map);
-
-        this.map.addLayer(this.markersLayer);
-    }
-
-    private buildMarkerTemplate(name: string): string {
-        return `
-    <div class="custom-marker">
-      <div class="dot"></div>
-      <img src="assets/bird.svg" alt="Marker image" width="30" height="30"/>
-    </div>
-  `;
-    }
-
-    private loadMarkers(): void {
-        const data = this.data().map((addr) => ({
-            lat: addr.latitude!,
-            lng: addr.longitude!,
-            name: addr.street || 'Adresse'
-        }));
-
-        this.markersLayer.clearLayers();
-
-        data.forEach((p) => {
-            const icon = L.divIcon({
-                html: this.buildMarkerTemplate(p.name),
-                className: '',
-                iconSize: [30, 30],
-                iconAnchor: [15, 30]
-            });
-            const marker = L.marker([p.lat, p.lng], { icon });
-            this.markersLayer.addLayer(marker);
+    constructor() {
+        effect(() => {
+            const url = this.mapUrl();
+            if (this.map) {
+                this.map.setStyle(url);
+            }
         });
     }
 
-    private fitMapToMarkers(): void {
-        if (!this.map) return;
-        const bounds = this.markersLayer.getBounds();
-        if (bounds.isValid() && this.secondaryAddress()) {
-            this.map.fitBounds(bounds, { padding: [50, 50] });
-        } else {
-            this.map.setView([this.mainAddress().latitude!, this.mainAddress().longitude!], 13);
-        }
+    ngAfterViewInit(): void {
+        this.map = new maplibregl.Map({
+            container: 'map',
+            style: this.mapUrl(),
+            center: [this.longitude(), this.latitude()],
+            zoom: this.zoom
+        });
+
+        new maplibregl.Marker().setLngLat([this.longitude(), this.latitude()]).addTo(this.map);
     }
 
     ngOnDestroy(): void {
