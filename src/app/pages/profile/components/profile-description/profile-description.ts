abe1cc27af94e2e7db34b8f93003d6796b16314e
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-01-15 12:31:02 +0100

    security  fail: script inject

--- src/app/pages/profile/components/profile-description/profile-description.ts
+++ src/app/pages/profile/components/profile-description/profile-description.ts
@@ -5,20 +5,18 @@ import { Button } from 'primeng/button';
 import { Card } from 'primeng/card';
 import { Tooltip } from 'primeng/tooltip';
 import { UserDetails } from 'src/client';
+import { SafeHtmlPipe } from '../../../shared/pipes/safe-html-pipe';
 
 @Component({
     selector: 'bp-profile-description',
-    imports: [Card, Button, Tooltip],
+    imports: [Card, Button, Tooltip, SafeHtmlPipe],
     templateUrl: './profile-description.html'
 })
 export class ProfileDescription {
     mainService = inject(MainService);
     user = input.required<UserDetails>();
     sanitizer = inject(DomSanitizer);
-    descriptionSafeHtml = computed<SafeHtml>(() => {
-        const description = this.user()?.description || 'Pas de description fournie';
-        return this.sanitizer.bypassSecurityTrustHtml(description.replace(/\n/g, '<br>'));
-    });
+
     languages = computed(() => {
         return this.user()?.languages || [];
     });
