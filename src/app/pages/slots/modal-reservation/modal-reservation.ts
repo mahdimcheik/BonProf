244122f8ab129fd997e4c2386870cfb2c89d493d
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-01-18 11:05:20 +0100

    conform delete or accpet modal

--- src/app/pages/slots/modal-reservation/modal-reservation.ts
+++ src/app/pages/slots/modal-reservation/modal-reservation.ts
@@ -1,16 +1,17 @@
 import { BaseModalComponent } from '@/pages/components/base-modal/base-modal.component';
 import { CalendarEvent } from '@/pages/shared/models/calendar-models';
-import { Component, computed, inject, model, OnInit, output } from '@angular/core';
+import { Component, computed, inject, model, OnInit, output, signal } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { TagModule } from 'primeng/tag';
 import { ReservationDetails, SlotDetails, StatusReservationCode } from 'src/client';
 import { SlotWrapperService } from '@/pages/shared/services/slot-wrapper-service';
+import { ConfirmModalComponent } from '@/pages/components/confirm-modal/confirm-modal.component';
 
 @Component({
     selector: 'bp-modal-reservation',
-    imports: [BaseModalComponent, ButtonModule, AvatarModule, TagModule, DatePipe],
+    imports: [BaseModalComponent, ButtonModule, AvatarModule, TagModule, DatePipe, ConfirmModalComponent],
     templateUrl: './modal-reservation.html'
 })
 export class ModalReservation implements OnInit {
@@ -19,6 +20,16 @@ export class ModalReservation implements OnInit {
     event = model.required<CalendarEvent>();
     slot = computed<SlotDetails>(() => this.event()?.ExtendedProps?.['slot'] ?? null);
     reservation = computed(() => this.slot()?.reservation ?? null);
+
+    // Confirmation modal properties
+    showConfirmModal = model(false);
+    isConfirmModal = signal(false);
+    question = computed(() =>
+        this.isConfirmModal()
+            ? `Êtes-vous sûr de vouloir confirmer la réservation de ${this.reservation()?.student?.user?.firstName} ${this.reservation()?.student?.user?.lastName}`
+            : `Êtes-vous sûr de vouloir supprimer la réservation de ${this.reservation()?.student?.user?.firstName} ${this.reservation()?.student?.user?.lastName}`
+    );
+    titleModal = computed(() => (this.isConfirmModal() ? 'Annuler la réservation' : 'Confirmer la réservation'));
 
     public pending = StatusReservationCode.Pending;
     public accepted = StatusReservationCode.Accepted;
@@ -37,18 +48,30 @@ export class ModalReservation implements OnInit {
     removeClicked = output<ReservationDetails>();
 
     onUpdate() {
-        this.updateClicked.emit(this.reservation());
+        this.isConfirmModal.set(true);
+        this.showConfirmModal.set(true);
     }
 
     onRemove() {
-        if (!this.reservation()) {
-            this.visible.set(false);
-            return;
-        }
-        this.removeClicked.emit(this.reservation()!);
+        this.isConfirmModal.set(false);
+        this.showConfirmModal.set(true);
     }
 
     cancel() {
         this.visible.set(false);
+    }
+
+    // Confirmation modal actions
+    confirm() {
+        this.showConfirmModal.set(false);
+        if (this.isConfirmModal()) {
+            this.updateClicked.emit(this.reservation());
+        } else {
+            if (!this.reservation()) {
+                this.visible.set(false);
+                return;
+            }
+            this.removeClicked.emit(this.reservation()!);
+        }
     }
 }
