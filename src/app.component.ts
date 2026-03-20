941ad6bddf4a8358ed94a129d6c2b26fc98215e6
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-03-20 01:20:29 +0100

    connection sgnalR Ok

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { OverlaySpinner } from '@/pages/components/overlay-spinner/overlay-spinner';
+import { MainService } from '@/pages/shared/services/main.service';
+import { SignalRService } from '@/pages/shared/services/signal-r-service';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Toast } from 'primeng/toast';
-import { OverlaySpinner } from '@/pages/components/overlay-spinner/overlay-spinner';
 
 @Component({
     selector: 'app-root',
@@ -10,4 +12,13 @@ import { OverlaySpinner } from '@/pages/components/overlay-spinner/overlay-spinn
     template: ` <bp-overlay-spinner></bp-overlay-spinner>
         <p-toast position="top-center"> </p-toast><router-outlet> </router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    signalRService = inject(SignalRService);
+    mainService = inject(MainService);
+
+    ngOnInit(): void {
+        if (this.mainService.token()) {
+            this.signalRService.initiateAndConnect(this.mainService.token());
+        }
+    }
+}
