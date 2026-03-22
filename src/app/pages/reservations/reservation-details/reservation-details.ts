f70ff4527228d313147c9e27d71b532ce35be478
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-03-22 21:56:36 +0100

    chat wip

--- src/app/pages/reservations/reservation-details/reservation-details.ts
+++ src/app/pages/reservations/reservation-details/reservation-details.ts
@@ -5,18 +5,34 @@ import { firstValueFrom } from 'rxjs';
 import { ReservationDetails } from 'src/client';
 import { SplitterModule } from 'primeng/splitter';
 import { ButtonModule } from "primeng/button";
+import { MainService } from '@/pages/shared/services/main.service';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
+import { TextareaModule } from 'primeng/textarea';
+import { InputTextModule } from 'primeng/inputtext';
 
+export interface ChatMessage {
+    userId: string;
+    author: string;
+    content: string;
+    sentAt: Date; 
+    timestamp: Date;
+}
 @Component({
   selector: 'bp-reservation-details',
-  imports: [SplitterModule, ButtonModule],
+  imports: [SplitterModule, ButtonModule, DatePipe, FormsModule,InputTextModule, TextareaModule, TooltipModule],
   templateUrl: './reservation-details.html',
 })
 export class ReservationDetailsPage  implements OnInit {
   slotService = inject(SlotWrapperService);
   activatedRoute = inject(ActivatedRoute);
+  mainService = inject(MainService);
   reservationId = signal<string>('');
-  height = signal<string>(window.screen.height - 250 + 'px');
+  height = signal<string>(window.screen.height - 200 + 'px');
   panelSizes = signal<[number, number]>([25, 75]);
+  messages = signal<ChatMessage[]>([]);
+  newMessage = signal<string>('');
 
   reservation = signal<ReservationDetails | undefined>(undefined);
   ngOnInit(): void {
@@ -43,4 +59,20 @@ export class ReservationDetailsPage  implements OnInit {
       this.panelSizes.set([99.9, 0.1]);
     }
   }
+
+      isCurrentUserMessage(message: ChatMessage): boolean {
+        const currentUser = this.mainService.userConnected();
+        return currentUser.id === message.userId;
+    }
+
+        onKeyDown(event: KeyboardEvent) {
+        if (event.ctrlKey && event.key === 'Enter') {
+            event.preventDefault();
+            this.sendMessage();
+        }
+    }
+
+    async sendMessage() {
+        
+    }
 }
